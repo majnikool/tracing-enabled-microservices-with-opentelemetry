f24ad8fd7db1b9f84bea3fcfc8c182781691c0ef
eteghma <majid.eteghad@tecnotree.com>
2024-07-06 22:19:15 +0000

    test

--- source_code/setup.py
+++ source_code/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="example-app",
-    version="0.1.0",
+    version="0.2.0",
     author="Majid",
     author_email="majidnik@gmail.com",
     description="An example app",
